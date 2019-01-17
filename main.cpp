37ad498bdddedea7b406b49a371f47de0b505d85
gilberto <g_torres1983@hotmail.com>
2019-01-17 01:07:47 -0500

    initial commit game demineur

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: GT
+ *
+ * Created on 17 janvier 2019, 00:52
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout<<"Test github c++";
+    return 0;
+}
+
